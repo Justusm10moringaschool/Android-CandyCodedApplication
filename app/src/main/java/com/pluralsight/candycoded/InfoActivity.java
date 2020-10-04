57d7f3e284fcc800e861783ea07aa391e6576697
Justus Dev <56016813+Justusm10moringaschool@users.noreply.github.com>
2020-10-04 12:18:48 +0300

    completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,13 +1,16 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,23 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
+    // *** Task 2 to launch the google Maps Activity
+
+    public void createMapIntent(View view) {
+        Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW,
+            Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801"));
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
+
+    }
+
+
+
+
+    //
     // ***
 
     // ***
