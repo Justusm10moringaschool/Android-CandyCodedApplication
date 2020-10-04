5b70601f7ac9b7301c6945a0e90c1c67db7529ec
Justus Dev <56016813+Justusm10moringaschool@users.noreply.github.com>
2020-10-04 10:43:09 +0300

    Update MainActivity.java
    Add an Intent to launch the infoActivity from MainActivity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -79,6 +80,19 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main, menu);
         return true;
     }
+
+    // Task 1 - Show Store Information Activity
+    
+   // implemetation of launching an intent of InfoActivity
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent infoIntent = new Intent(this, InfoActivity.class);
+        startActivity(infoIntent);
+        return super.onOptionsItemSelected(item);
+    }
+
+    // close the candy coded to launch intent
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
